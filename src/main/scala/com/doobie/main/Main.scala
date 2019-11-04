939571ab2da2bc07c2c39dc3f9c66b2498b5144d
NzDush <dushnz@gmail.com>
2019-11-04 11:55:27 +0530

    commented method calls

--- src/main/scala/com/doobie/main/Main.scala
+++ src/main/scala/com/doobie/main/Main.scala
@@ -12,28 +12,28 @@ object Main extends App with MysqlConnection{
   import com.doobie.controller.ManufacturerController.{connectionBetweenEmployeeAssetManufacturer, connectionBetweenProjectEmployeeAssetManufacturer}
   import com.doobie.controller.ProjectController.employeeWithProjects
 
-  insertOneEmployee1(Employee(22,"MMM","Galle",86.3))
-
-  insertManyEmployees(List(Employee(32,"MMM","Galle",86.3), Employee(33,"MMM","Galle",86.3), Employee(34,"MMM","Galle",86.3)))
-
-  viewAllEmployees
-
-  updateEmployeeViaId(32, Employee(22,"NNN","Nuwara-Eliya",65.2))
-
-  deleteEmployeeViaId(34)
-
-  groupEmployeesByAddress
-
-  sortEmployeeBySalary
-
-  sortEmployeeNamesBySalaryWithMapping
-
-  employeeAssetsWithFilterSalary(50)
-
-  connectionBetweenEmployeeAssetManufacturer
-
-  connectionBetweenProjectEmployeeAssetManufacturer
-
-  employeeWithProjects
+//  insertOneEmployee1(Employee(22,"MMM","Galle",86.3))
+//
+//  insertManyEmployees(List(Employee(32,"MMM","Galle",86.3), Employee(33,"MMM","Galle",86.3), Employee(34,"MMM","Galle",86.3)))
+//
+//  viewAllEmployees
+//
+//  updateEmployeeViaId(32, Employee(22,"NNN","Nuwara-Eliya",65.2))
+//
+//  deleteEmployeeViaId(34)
+//
+//  groupEmployeesByAddress
+//
+//  sortEmployeeBySalary
+//
+//  sortEmployeeNamesBySalaryWithMapping
+//
+//  employeeAssetsWithFilterSalary(50)
+//
+//  connectionBetweenEmployeeAssetManufacturer
+//
+//  connectionBetweenProjectEmployeeAssetManufacturer
+//
+//  employeeWithProjects
 
 }
